631148987ed7ead11c0be18ccbc3181690293a5a
OmegaJuan <bigcd63@gmail.com>
2026-03-09 01:31:52 +0700

    Delete src directory

--- src/routes/homeRoutes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import * as homeController from "../controllers/homeController";
-
-const router = Router();
-
-router.get("/", homeController.getHome);
-
-export default router;
