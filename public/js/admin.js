9428d6e9e179efea0726c1bd7859fa6fcdf00625
OmegaJuan <bigcd63@gmail.com>
2026-03-09 01:31:14 +0700

    Delete public directory

--- public/js/admin.js
+++
@@ -1,7 +0,0 @@
-function openAdminModal() {
-    document.getElementById("adminModal").style.display = "flex";
-}
-
-function closeAdminModal() {
-    document.getElementById("adminModal").style.display = "none";
-}
